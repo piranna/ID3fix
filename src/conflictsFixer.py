ed82f2bda69d3eab0d5cae6f04ed15b33716f1d4
Jesús Leganés Combarro "piranna" <piranna@gmail.com>
2013-08-19 00:03:20 +0200

    Added tool to fix renamed files due to case conflicts

---
+++ src/conflictsFixer.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''
+Created on 18/08/2013
+
+@author: piranna
+'''
+
+from itertools import count
+from os        import renames
+from os.path   import exists, join, splitext
+from re        import sub
+
+
+CONFLICT_REGEX = [r' \(Case Conflict( \(\d+\))?\)',
+                  r' \(Conflicto de mayúsculas y minúsculas( \(\d+\))?\)']
+
+
+def genNonConflictingPath(path):
+    root, ext = splitext(path)
+
+    for index in count(1):
+        newPath = root+' ('+str(index)+')'+ext
+
+        if not exists(newPath):
+            return newPath
+
+
+def fixConflict(root, oldPath):
+    path = oldPath
+
+    for regex in CONFLICT_REGEX:
+        path = sub(regex, '', path)
+
+    newPath = join(root, path)
+
+    if newPath == oldPath:
+        return
+
+    if exists(newPath):
+        newPath = genNonConflictingPath(newPath)
+
+    renames(oldPath, newPath)
+
+    print oldPath
+    print newPath
+    print
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    from os       import walk
+    from os.path  import abspath
+
+    parser = ArgumentParser()
+    parser.add_argument('dirs', nargs='+')
+
+    args = parser.parse_args()
+
+    root = abspath(args.dirs[0])
+
+    for arg_dir in args.dirs:
+        for dirpath, dirnames, filenames in walk(arg_dir):
+            for name in filenames:
+                fixConflict(root, join(dirpath, name))
