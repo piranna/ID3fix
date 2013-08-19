a9145908cd5d7198280453eddf5acfdeb9b3a112
Jesús Leganés Combarro "piranna" <piranna@gmail.com>
2013-08-19 12:15:24 +0200

    Allow to use relative & absolute paths and ignore rename of conflicts

--- src/conflictsFixer.py
+++ src/conflictsFixer.py
@@ -19,46 +19,55 @@ CONFLICT_REGEX = [r' \(\d+\)',
                   r' \(Copia en conflicto de .+? \d{4}-\d{2}-\d{2}\)']
 
 
-def genNonConflictingPath(path):
-    root, ext = splitext(path)
+def genNonConflictingPath(newPath, oldPath):
+    if newPath == oldPath:
+        return
+
+    root, ext = splitext(newPath)
 
     for index in count(1):
         newPath = root+' ('+str(index)+')'+ext
+
+        if newPath == oldPath:
+            return
 
         if not exists(newPath):
             return newPath
 
 
 def fixConflict(root, oldPath):
-    path = oldPath
+    path = relpath(oldPath, root)
 
     for regex in CONFLICT_REGEX:
         path = sub(regex, '', path)
 
     newPath = join(root, path)
 
-    if newPath == oldPath:
-        return
-
     if exists(newPath):
-        newPath = genNonConflictingPath(newPath)
+        newPath = genNonConflictingPath(newPath, oldPath)
 
-    renames(oldPath, newPath)
+    if newPath:
+        #renames(oldPath, newPath)
 
-    print '[Renamed]',oldPath,'=>',newPath
+        print '[Renamed]',oldPath,'=>',newPath
 
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
     from os       import removedirs, walk
-    from os.path  import abspath
+    from os.path  import abspath, commonprefix, dirname, relpath
 
     parser = ArgumentParser()
     parser.add_argument('dirs', nargs='+')
 
     args = parser.parse_args()
 
-    root = abspath(args.dirs[0])
+    # Set all paths as absolute ones
+    for index, arg_dir in enumerate(args.dirs):
+        args.dirs[index] = abspath(arg_dir)
+
+    root = dirname(commonprefix(args.dirs))
+    print '[Root]',root
 
     for arg_dir in args.dirs:
         for dirpath, dirnames, filenames in walk(arg_dir):
@@ -67,13 +76,13 @@ if __name__ == '__main__':
             for name in filenames:
                 fixConflict(root, join(dirpath, name))
 
-            # Empty directories
-            for name in dirnames:
-                path = join(dirpath, name)
-
-                try:
-                    removedirs(path)
-                except OSError:
-                    pass
-                else:
-                    print '[Deleted]',path
+#             # Empty directories
+#             for name in dirnames:
+#                 path = join(dirpath, name)
+#
+#                 try:
+#                     removedirs(path)
+#                 except OSError:
+#                     pass
+#                 else:
+#                     print '[Deleted]',path
